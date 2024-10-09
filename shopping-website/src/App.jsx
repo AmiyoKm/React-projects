30d32c7d570b4a148f5fd99c8b77596660a7144c
AmiyoKm <omimondol18@gmail.com>
2024-10-10 00:54:04 +0600

    Rendering Products done

---
+++ shopping-website/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home'
+import Cart from './pages/cart'
+import Header from './component/header'
+function App() {
+
+
+  return (
+    <>
+    <Header />
+      <Routes>
+      <Route exact path="/" element={<Home/>}/>
+      <Route exact path="/cart" element={<Cart/>}/>
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ shopping-website/src/pages/cart.jsx
@@ -0,0 +1,11 @@
+
+
+
+export default function Cart(){
+    return <div>
+        <h1>Cart</h1>
+        <p>Your shopping cart is empty.</p>
+        <button>Checkout</button>
+        <button>Continue Shopping</button>
+    </div>
+}
