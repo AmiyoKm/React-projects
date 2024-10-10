6384e0a003199863bd44d9d99d42fe79db4a38ee
AmiyoKm <omimondol18@gmail.com>
2024-10-10 14:14:42 +0600

    Product shopping home page and cart page done

--- shopping-website/src/component/product-tile/index.jsx
+++ shopping-website/src/component/product-tile/index.jsx
@@ -1,8 +1,19 @@
 
-
+import { useDispatch, useSelector } from "react-redux"
+import { addToCart, removeFromCart } from "../../store/slices/cart-slice";
 
 
 export default function ProductTile({product}){
+    const dispatch = useDispatch();
+    const {cart} = useSelector(state=>state)
+    function handleAddToCart(){
+        dispatch(addToCart(product))
+    }
+    function handleRemoveFromCart(){
+     dispatch(removeFromCart(product.id))
+     console.log("Removed product")
+    }
+
     return <div className="group flex flex-col items-center border-2 border-red-900 gap-3 p-4 h-[360px] mt-10 ml-5 rounded-xl">
         <div className="h-[180px]">
             <img src={product?.image} alt={product?.title} className="object-cover h-full w-full" />
@@ -14,7 +25,12 @@ export default function ProductTile({product}){
                 </h1>
             </div>
             <div className="flex items-start justify-center w-full mt-5">
-                <button className="bg-red-950 text-white   border-2 rounded-lg font-bold p-4">Add to Cart</button>
+                <button onClick={cart.some(item=> item.id===product.id) ? handleRemoveFromCart:handleAddToCart}className="bg-red-950 text-white   border-2 rounded-lg font-bold p-4">
+                    {
+                        cart.some(item=> item.id===product.id) ? "Remove from Cart" : "Add to Cart"
+                    }
+                    </button>
+
             </div>
         </div>
        
--- shopping-website/src/pages/cart.jsx
+++ shopping-website/src/pages/cart.jsx
@@ -1,11 +1,49 @@
+import { useEffect, useState } from "react"
+import { DiGoogleDrive } from "react-icons/di";
+import { useSelector } from "react-redux"
+import { Link } from "react-router-dom";
+import CartTile from "../component/cart-tile";
 
 
 
 export default function Cart(){
-    return <div>
-        <h1>Cart</h1>
-        <p>Your shopping cart is empty.</p>
-        <button>Checkout</button>
-        <button>Continue Shopping</button>
-    </div>
+    const [totalCart , setTotalCart] = useState(null);
+    const {cart} =  useSelector(state=> state);
+
+    useEffect(()=>{
+        setTotalCart(cart.reduce((acc,curr)=>acc+curr.price,0))
+    },[cart])
+    console.log(cart)
+    return <>
+        {
+            cart && cart.length>0?
+                <div className="min-h-[80vh] grid md:grid-cols-2 max-w-6xl mx-auto">
+                        <div className="flex flex-col justify-center items-center p-3 ">
+                            {
+                                cart.map((item,index)=> <CartTile key={index}cartItem={item}/> )
+                            }
+                        </div>
+                        <div>
+                            <div className="flex flex-col justify-center items-end p-6 space-y-5 mt-14 ">
+                                <h1 className="font-bold text-lg text-red-800">Your Cart Summary</h1>
+                                <p>
+                                    <span className="text-gray-800 font-bold">Total Item</span>
+                                    <span>: {cart.length}</span>
+                                </p>
+                                <p>
+                                <span className="text-gray-800 font-bold">Total amount</span>
+                                <span>: {totalCart}</span>
+                                </p>
+                            </div>
+                        </div>
+                </div> : <div className="min-h-[80vh] flex flex-col items-center justify-center">
+                        <h1 className="text-gray-800 font-bold text-xl mb-2">Your Cart is Empty</h1>
+                        <Link to={"/"}>
+                            <button className="bg-red-950 text-white   border-2 rounded-lg font-bold p-4">
+                                Shop Now
+                            </button>
+                        </Link>
+                </div>
+        }
+    </>
 }
